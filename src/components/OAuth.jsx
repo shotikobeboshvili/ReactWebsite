bf5370a0329bd0a483725d12db0667a64e03d30f
shotikobeboshvili <beboshvilishotiko61@gmail.com>
2024-12-07 15:52:31 +0400

    create Auth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,44 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, serverTimestamp } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
-import React from 'react'
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { getDoc, setDoc } from "firebase/firestore/lite";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+    const navigate = useNavigate()
+   async function onGoogleClick() {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup( auth, provider)
+            const user = result.user;
+            
+            
+            //check for the user 
+
+            const docRef = doc(db, "user", user.uid)
+            const  docSnap = await getDoc(docRef)
+
+            if(!docSnap.exists()) {
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                });
+            }
+
+            navigate('.../')
+        } catch (error) {
+            toast.error("Could not authorized with google")
+            console.log(error);
+            
+        }
+    }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700
+    <button type="button"
+     onClick={onGoogleClick} className="flex items-center justify-center w-full bg-red-700
      text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 
      active:bg-red-900 shadow-md hover:shadow-lg transition duration-150 
      ease-in-out rounded">
